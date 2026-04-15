778452bcc5a3907bf87a597f839f06f2d49fae99
remieClaire <remie-claire-danielle.enriquez.090@my.csun.edu>
2026-04-14 22:53:50 -0700

    finished setting up layout and adding images

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
-fetch("https://pokeapi.co/api/v2/berry/rawst/")
-    // once promise resolves, we'll get a response object
-    .then(response => console.log(response))
-    // for catching errors
-    .catch(error => console.error(error));
+
+async function fetchData() {
+    try {
+        const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+        
+        const POKEMON_URL = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`;
+        const response = await fetch(POKEMON_URL);
+
+        if (!response.ok) {
+            throw new Error("Could not fetch resource");
+        }
+
+        const data = await response.json();
+        console.log(data);
+        
+        // get sprite
+        const pokemonSprite = data.sprites.front_default;
+        const imgElement = document.getElementById("pokemonSprite");
+
+        // load berry sprite into source
+        imgElement.src = pokemonSprite;
+        // change image style to block
+        imgElement.style.display = "block";
+        
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
