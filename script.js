127b8757358b5fa37aaae3dfacf64eb26d84ad10
remieClaire <remie-claire-danielle.enriquez.090@my.csun.edu>
2026-04-16 13:31:55 -0700

    finished layout

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-const API_URL = `https://ghibliapi.dev/films`;
+
 
 async function fetchImg() {
     try {
+        const API_URL = `https://ghibliapi.dev/films`;
+
         const response = await fetch(API_URL);
 
         if (!response.ok) {
@@ -9,23 +11,38 @@ async function fetchImg() {
         }
 
         const data = await response.json();
+        
+        let count = 0;
 
         //using foreach, parse through data and grab images
+        data.forEach( (item) => {
+
+            //grab data img
+            const poster = item.image; 
+
+            //array of button elements to put pictures in for
+            const btnId = ["nausicaa-img", "castle-img", "grave-img", "totoro-img", "kiki-img", "yesterday-img", "porco-img", "ocean-img", "pom-img", "whisper-img", "mononoke-img", "yamada-img", "spirited-img", "cat-img", "howl-img", "earthsea-img", "ponyo-img", "arrietty-img", "poppy hill-img", "wind-img", "kaguya-img", "marnie-img", "red turtle-img", "earwig-img", "heron-img"];
+
+            //get the corresponding input id to put the image into
+            const element = document.getElementById(btnId[count]);
+            //set the image
+            element.src = poster;
+
+            count++;
+        });
+
+        
 
 
     }
     catch (error) {
         console.error("Error:", error.message);
     }
-    
-
-
-
-    var idArr = [];
 }
 
+/*
 async function fetchData() {
-    /* ----------- variables ----------- */
+    /* ----------- variables ----------- 
     //setting URL source
     const URL = `https://ghibliapi.dev/films/86e544fd-79de-4e04-be62-5be67d8dd92e`;
 
@@ -42,7 +59,7 @@ async function fetchData() {
 
         //get data from json file response
         const data = await response.json();
-        console.log(data);
+        //console.log(data);
 
         //get image 
         const nausicaaPoster = data.image;
@@ -58,5 +75,7 @@ async function fetchData() {
         console.error("Error:", error.message);
     }
 }
+*/
 
-fetchData();
+//fetchData();
+fetchImg();
