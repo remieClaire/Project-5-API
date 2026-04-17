45f25093f212a0b0c4112b2d06614ea16ada9317
remieClaire <remie-claire-danielle.enriquez.090@my.csun.edu>
2026-04-16 17:40:57 -0700

    added dialog modal functionality
    successfully pulls background image

--- script.js
+++ script.js
@@ -1,9 +1,63 @@
+const API_URL = `https://ghibliapi.dev/films`;
 
+//array of button elements to put pictures in for
+const btnId = ["nausicaa-img", "castle-img", "grave-img", "totoro-img", "kiki-img", "yesterday-img", "porco-img", "ocean-img", "pom-img", "whisper-img", "mononoke-img", "yamada-img", "spirited-img", "cat-img", "howl-img", "earthsea-img", "ponyo-img", "arrietty-img", "poppy hill-img", "wind-img", "kaguya-img", "marnie-img", "red turtle-img", "earwig-img", "heron-img"];
+
+const dialog = document.getElementById("dialog");
+const dialogWrapper = document.querySelector(".dialog-wrapper");
+
+function registerInput() {
+    const allInput = document.querySelectorAll('input');
+
+    //register click event for inputs
+    allInput.forEach( (input) => {
+        input.addEventListener('click', (e) => {
+            fetchDetails(e.target.id);
+        });
+    });
+}
+
+function showMovieDetails() {
+    fetchDetails();
+    dialog.showModal();
+}
+
+let elementIndex = 0;
+
+async function fetchDetails(input) {
+    try {
+        //getting response from server
+        const response = await fetch(API_URL);
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        let index = 0;
+        
+        //match input to id in button array
+        btnId.forEach( (btn) => {
+            if (btn == input) {
+                //getting index of button
+                elementIndex = index;
+                return;
+            }
+            index++;
+        });
+
+        const data = await response.json();
+        
+        const banner = data[elementIndex].movie_banner;
+
+        dialog.style.backgroundImage = `url('${banner}')`;
+    }
+    catch (error) {
+        console.error("Error:", error.message);
+    }
+}
 
 async function fetchImg() {
     try {
-        const API_URL = `https://ghibliapi.dev/films`;
-
         const response = await fetch(API_URL);
 
         if (!response.ok) {
@@ -19,9 +73,6 @@ async function fetchImg() {
 
             //grab data img
             const poster = item.image; 
-
-            //array of button elements to put pictures in for
-            const btnId = ["nausicaa-img", "castle-img", "grave-img", "totoro-img", "kiki-img", "yesterday-img", "porco-img", "ocean-img", "pom-img", "whisper-img", "mononoke-img", "yamada-img", "spirited-img", "cat-img", "howl-img", "earthsea-img", "ponyo-img", "arrietty-img", "poppy hill-img", "wind-img", "kaguya-img", "marnie-img", "red turtle-img", "earwig-img", "heron-img"];
 
             //get the corresponding input id to put the image into
             const element = document.getElementById(btnId[count]);
@@ -40,42 +91,13 @@ async function fetchImg() {
     }
 }
 
-/*
-async function fetchData() {
-    /* ----------- variables ----------- 
-    //setting URL source
-    const URL = `https://ghibliapi.dev/films/86e544fd-79de-4e04-be62-5be67d8dd92e`;
-
-    try {
-        
-        //getting response from URL
-        const response = await fetch(URL);
-        
-        console.log("Status:", response.status);
-
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-
-        //get data from json file response
-        const data = await response.json();
-        //console.log(data);
-
-        //get image 
-        const nausicaaPoster = data.image;
-        //get movie slot element
-        const nausicaaElement = document.getElementById("nausicaa-img");
-
-        //set image
-        nausicaaElement.src = nausicaaPoster;
-        nausicaaElement.style.display = "block";
-
-    }
-    catch (error) {
-        console.error("Error:", error.message);
-    }
-}
-*/
-
-//fetchData();
 fetchImg();
+registerInput();
+
+//for closing dialog
+dialog.addEventListener("click", (e) => {
+    //close dialog so long as content outside of wrapper is clicked
+    if (!dialogWrapper.contains(e.target)) {
+        dialog.close();
+    }
+});
